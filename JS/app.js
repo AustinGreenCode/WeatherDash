5f483116a9ea08d1b48be2708dd07635bff6cc61
austin green <trentgr33n@icloud.com>
2022-05-28 17:03:24 -0400

    amlost finsished/few problem left to fix

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 const ft = new fetch();
-const ui = new UI();
+var ui = new UI();
 
 const search = document.getElementById("searchUser");
 const button = document.getElementById("submit");
@@ -7,13 +7,13 @@ button.addEventListener("click", () => {
     const currentVal = search.value;
 
     ft.getCurrent(currentVal).then((data) => {
-        ui.populateUI(data);
-        ui.saveToLS(data);
+        UI.populateUI(data);
+        UI.saveToLS(data);
     
     });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    const dataSaved = ui.getFromLS();
-    ui.populateUI(dataSaved);
+    const dataSaved = UI.getFromLS();
+    UI.populateUI(dataSaved);
 });
